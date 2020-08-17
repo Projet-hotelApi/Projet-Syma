a284a9a597d8854c372f3aa3743378bae68adea1
Mathilde Dehedin <mathilde@macbook-pro.home>
2020-08-17 20:35:07 +0200

    first commit

---
+++ model/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const User = mongoose.model("User", {
+  token: String,
+  hash: String,
+  salt: String,
+  email: { type: String, unique: true },
+  username: { type: String, required: true },
+  description: String,
+  picture: String,
+  globalNote: Number,
+  reviews: {
+    ratingValue: Number,
+    description: String,
+  },
+  personnal: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    postalCode: { type: Number, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+  },
+  articles: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ad",
+    },
+  ],
+});
+module.exports = User;
